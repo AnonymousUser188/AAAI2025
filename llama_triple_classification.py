0e475cd7e3869c4409b7130d8ef4261680f6b45e
AnonymousUser188 <1424190634@qq.com>
2024-09-13 09:28:53 +0800

    Add files via upload

--- llama_triple_classification.py
+++ llama_triple_classification.py
@@ -77,10 +77,6 @@ labels = []
 count = 0
 correct_count = 0
 
-print("=====================================================")
-
-correct_line = []
-wrong_line = []
     
 with open(test_file, "r", encoding="utf-8") as f:
     lines = f.readlines()
@@ -104,24 +100,17 @@ with open(test_file, "r", encoding="utf-8") as f:
 
         count += 1
         label = tmp[1]
-        if ans.find("yes") != -1 and label == "1":
-            correct_count += 1
-            correct_line.append(line)
+        # if ans.find("yes") != -1 and label == "1":
+        #     correct_count += 1
+        # elif (ans.find("false") != -1 or ans.find("not") != -1 or ans.find("n't") != -1 or ans.find("no") != -1) and label == "-1":
+        #     correct_count += 1
+        # print(prompt)
+        # print("---------------------------")
+        # print("\n",correct_count/count,"\n")
+        # print("---------------------------")
+        # print(ans)
+        # print(label)
 
-        elif (ans.find("false") != -1 or ans.find("not") != -1 or ans.find("n't") != -1 or ans.find("no") != -1) and label == "-1":
-            correct_count += 1
-            correct_line.append(line)
-
-        else:
-            wrong_line.append(line)   
-
-        print(prompt)
-        print("---------------------------")
-        print("\n",correct_count/count,"\n")
-        print("---------------------------")
-        print(ans)
-        print(label)
-        print("==============================================")
         lines_to_write.append(prompt+"\t"+ans.replace("\n",".")+"\t"+ label+"\n")
 
 
@@ -133,92 +122,12 @@ with open(res_file, "w", encoding="utf-8") as f:
 
 
 
-correct_file = test_file.split(".")[0].replace("data", "test")+"-"+model_id.split("/")[-1]+"-"+str(time.strftime('%m%d_%H%M',time.localtime())) +"-correct.csv"
-wrong_file = test_file.split(".")[0].replace("data", "test")+"-"+model_id.split("/")[-1]+"-"+str(time.strftime('%m%d_%H%M',time.localtime())) +"-wrong.csv"
-with open(correct_file, "w", encoding="utf-8") as f:
-    f.writelines(correct_line)
-with open(wrong_file, "w", encoding="utf-8") as f:
-    f.writelines(wrong_line)
 
 
-print("\n\n--------------\n")
-print("# ==============================================================")
-print("# 模型：", model_id)
-print("# 提示：", system_content)
-print("# 测试文件：", test_file)
-print("# 结果文件：", res_file)
+
+print("# ", model_id)
+print("# ", system_content)
+print("# ", test_file)
+print("# ", res_file)
 print("# ", len(lines), count, correct_count)
-print("# test quick acc:", correct_count/count)
-
-
-
-
-
-
-
-# ================================================================================================
-# ================================             FB13             ==================================
-# ================================================================================================
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Lora_Model/Llama/Llama-3-8B-Instruct-FB13-triple_neighbor-sft_0723
-# 测试文件： data/FB13/test_instructions_llama_neighbor_100.csv
-# 结果文件： output/FB13/test_instructions_llama_neighbor_100-Llama-3-8B-Instruct-FB13-triple_neighbor-sft_0723-0806_1611.csv
-#  101 100 65
-# test quick acc: 0.65
-
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Lora_plus_Model/Llama/Llama-3-8B-Instruct-wn11-triple-0718
-# 测试文件： data/FB13/test_instructions_llama_neighbor_100.csv
-# 结果文件： output/FB13/test_instructions_llama_neighbor_100-Llama-3-8B-Instruct-wn11-triple-0718-0808_2111.csv
-#  101 100 82
-# test quick acc: 0.82
-
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Model/Llama/Llama-3-8B-Instruct
-# 测试文件： data/FB13/test_instructions_llama_neighbor_100.csv
-# 结果文件： output/FB13/test_instructions_llama_neighbor_100-Llama-3-8B-Instruct-0808_2258.csv
-#  101 100 82
-# test quick acc: 0.82
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Model/Llama/Llama-3-8B-Instruct
-# 提示： You're an excellent knowledge graph analyst. You should answer yes or no to each question. When you are sure of the answer, give only "yes" or "no" and do not give any other words. When you are not sure about the answer, you can analyze the entity words in the question one by one and give your answer and explanation
-# 测试文件： data/FB13/test_instructions_llama_neighbor_100.csv
-# 结果文件： output/FB13/test_instructions_llama_neighbor_100-Llama-3-8B-Instruct-0808_2302.csv
-#  101 100 76
-# test quick acc: 0.76
-
-# ================================================================================================
-# ================================             WN11             ==================================
-# ================================================================================================
-
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Lora_plus_Model/Llama/Llama-3-8B-Instruct-wn11-triple-0718
-# 测试文件： data/WN11/test_instructions_llama_full-entity_neighbor_100.csv
-# 结果文件： output/WN11/pred_instructions_100_llama3-8B-0723_2049.csv
-# 101 100 94
-# test quick acc: 0.94
-
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Lora_plus_Model/Llama/Llama-3-8B-Instruct-wn11-triple-0718
-# 测试文件： data/WN11/test_instructions_llama_full-entity_neighbor_100.csv
-# 结果文件： output/WN11/test_instructions_llama_full-entity_neighbor_100-Llama-3-8B-Instruct-wn11-triple-0718-0806_1635.csv
-#  101 100 94
-# test quick acc: 0.94
-
-
-
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Pissa_Model/Llama/Llama-3-8B-Instruct-wn11-triple-0718
-# 测试文件： data/YAGO3-10/test_llama_triple_neighbor_full_100.csv
-# 结果文件： output/YAGO3-10/test_llama_triple_neighbor_full_100-Llama-3-8B-Instruct-wn11-triple-0718-0808_2138.csv
-#  100 99 61
-# test quick acc: 0.6161616161616161
-
-
-# ==============================================================
-# 模型： /home/user/chenqizhi/Data/Pissa_Model/Llama/Llama-3-8B-Instruct-FB13-triple_neighbor-sft_0718
-# 提示： You're an excellent knowledge graph analyst. You should answer yes or no to each question. When you are sure of the answer, give only "yes" or "no" and do not give any other words. When you are not sure about the answer, you can analyze the entity words in the question one by one and give your answer and explanation
-# 测试文件： data/WN11/test_instructions_llama_full-entity_neighbor_100.csv
-# 结果文件： output/WN11/test_instructions_llama_full-entity_neighbor_100-Llama-3-8B-Instruct-FB13-triple_neighbor-sft_0718-0808_2309.csv
-#  101 100 54
-# test quick acc: 0.54
+print("# ", correct_count/count)
